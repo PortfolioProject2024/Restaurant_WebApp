1c0f2f74154e2e3e89b29ab3cb9bbc3e11e197fd
Dawood Rizwan <dawood.rizwan@outlook.com>
2024-06-09 16:03:14 +0200

    User CRUD

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant_WebApp.Data;
+using Restaurant_WebApp.Repos.Interface;
 
 namespace Restaurant_WebApp.Controllers
 {
     public class UserController : Controller
     {
-
-
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+        private readonly IUserServices _userServices;
+        public UserController(ApplicationDbContext db, IUserServices userServices)
         {
-            return View();
+            _db = db;   
+            _userServices = userServices;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userList = _userServices.GetAllUserAsync();
+            return View(userList);
         }
     }
 }
