0aebce1f9a4fb85f12c464f62a1a2aa00e330ca9
NatalieAktas <151162812+NatalieAktas@users.noreply.github.com>
2024-06-14 15:33:11 +0200

    Revert "wip new"

---
+++ Models/ViewModels/MenuViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Restaurant_WebApp.Models.ViewModels;
+using System.Collections.Generic;
+
+namespace Restaurant_WebApp.Models.ViewModels
+{   
+    public class MenuViewModel
+    {
+        public List<FoodItem> FoodItems { get; set; }
+        public List<Category> Categories { get; set; }
+
+    }
+}
--- Repos/Services/UserServices.cs
+++ Repos/Services/UserServices.cs
@@ -1,146 +1,38 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Restaurant_WebApp.Data;
-using Restaurant_WebApp.Models;
-using Restaurant_WebApp.Models.ViewModels;
+﻿using Restaurant_WebApp.Models;
 using Restaurant_WebApp.Repos.Interface;
 
 namespace Restaurant_WebApp.Repos.Services
 {
-
     public class UserServices : IUserServices
     {
-        private readonly ApplicationDbContext _db;
-        private readonly UserManager<User> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
-
-        public UserServices(ApplicationDbContext db, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        public Task<User> DeleteUserAsync(string id)
         {
-            _db = db;
-            _userManager = userManager;
-            _roleManager = roleManager;
+            throw new NotImplementedException();
         }
 
-        public async Task<User> DeleteUserAsync(string id)
+        public Task<List<User>> GetAllUserAsync()
         {
-            var user = await _db.Users.FindAsync(id);
-            if (user == null)
-            {
-                return null;
-            }
-            _db.Users.Remove(user);
-            await _db.SaveChangesAsync();
-
-            return user;
-
+            throw new NotImplementedException();
         }
 
-        public async Task<List<UserWithRolesVM>> GetAllUserAsync()
+        public Task<List<User>> GetAllUsersInRoleAsync(string roleName)
         {
-            var userList = await _db.Users.Include(u => u.Customers).ToListAsync();
-            var userWithRoles = new List<UserWithRolesVM>();
-
-            foreach (var user in userList)
-            {
-                var roles = await _userManager.GetRolesAsync(user);
-                userWithRoles.Add(new UserWithRolesVM
-                {
-                    Id = user.Id,
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    Email = user.Email,
-                    Roles = roles.ToList(),
-                      Customers = user.Customers
-                });
-
-            }
-
-            return userWithRoles;
+            throw new NotImplementedException();
         }
 
-
-        public async Task<UserWithRolesVM> GetUserByIdAsync(string id)
+        public Task<User> GetUserByIdAsync(string id)
         {
-            // Fetch the user by id including related entities (e.g., Customers)
-            var user = await _db.Users.Include(u => u.Customers)
-                                      .FirstOrDefaultAsync(u => u.Id == id);
-
-            var roles = await _userManager.GetRolesAsync(user);
-
-            var userWithRolesVM = new UserWithRolesVM
-            {
-                Id = user.Id,
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                Email = user.Email,
-                Roles = roles.ToList(),
-                Customers = user.Customers
-            };
-
-
-            return userWithRolesVM;
+            throw new NotImplementedException();
         }
-                   
 
-        public async Task<bool> UpdateUserAsync(UserWithRolesVM modelVm)
+        public Task<bool> UpdateUserAsync(User user)
         {
-            var dbUser = await _db.Users.FindAsync(modelVm.Id);
-
-            if (dbUser == null)
-            {
-                return false; 
-            }
-
-            dbUser.FirstName = modelVm.FirstName;
-            dbUser.LastName = modelVm.LastName;
-            dbUser.Email = modelVm.Email;
-
-
-
-            _db.Users.Update(dbUser);
-            await _db.SaveChangesAsync();
-
-            return true;
+            throw new NotImplementedException();
         }
 
         public Task<string> UploadImageFileAsync(User user)
         {
             throw new NotImplementedException();
-        }
-
-
-        public async Task<List<IdentityRole>> GetAllRoles()
-        {
-            return await _roleManager.Roles.ToListAsync();
-        }
-
-
-        public async Task<List<string>> GetUserRolesAsync(string userId)
-        {
-            var user = await _userManager.FindByIdAsync(userId);
-            return user != null ? new List<string>(await _userManager.GetRolesAsync(user)) : new List<string>();
-
-        }
-
-        public async Task<bool> UpdateUserRolesAsync(string userId, string[] selectedRoles)
-        {
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                return false;
-            }
-            var currentRoles = await _userManager.GetRolesAsync(user);
-            var rolesToAdd = selectedRoles.Except(currentRoles).ToArray();
-            var rolesToRemove = currentRoles.Except(selectedRoles).ToArray();
-
-            var addResult = await _userManager.AddToRolesAsync(user, rolesToAdd);
-            if (!addResult.Succeeded)
-            {
-                return false;
-            }
-
-            var removeResult = await _userManager.RemoveFromRolesAsync(user, rolesToRemove);
-            return removeResult.Succeeded;
         }
     }
 }
