83ac349f840ec08654b5e52b88219db18d3a579a
Dawood Rizwan <dawood.rizwan@outlook.com>
2024-06-19 19:13:57 +0200

    contact class

---
+++ Repos/Interface/IContactService.cs
@@ -0,0 +1,17 @@
+﻿using Restaurant_WebApp.Models;
+
+namespace Restaurant_WebApp.Repos.Interface
+{
+    public interface IContactService 
+    {
+        Task<List<Contact>> GetAllMessagesAsync();
+
+        Task<Contact> DeleteMessageAsync(int id);
+
+        Task<Contact> GetMessageByIdAsync(int id);
+
+        Task AddMessageAsync(Contact contact);
+
+
+    }
+}
---
+++ Repos/Services/ContactService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant_WebApp.Data;
+using Restaurant_WebApp.Models;
+using Restaurant_WebApp.Repos.Interface;
+
+namespace Restaurant_WebApp.Repos.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ContactService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddMessageAsync(Contact contact)
+        {
+            _db.Contacts.Add(contact);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<Contact> DeleteMessageAsync(int id)
+        {
+            var del = await _db.Contacts.FindAsync(id);
+
+            _db.Contacts.Remove(del);
+            await _db.SaveChangesAsync();
+            return del;
+        }
+
+        public async Task<List<Contact>> GetAllMessagesAsync()
+        {
+            return await _db.Contacts.ToListAsync();
+        }
+
+        public async Task<Contact> GetMessageByIdAsync(int id)
+        {
+            return await _db.Contacts.FirstOrDefaultAsync(c => c.Id == id);
+        }
+    }
+}
